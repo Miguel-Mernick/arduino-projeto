559c4c9682b4b382ed348bf3c2119dd7b9b46ae5
Miguel Mernick <mernickmiguelsenai@gmail.com>
2024-08-05 15:10:38 -0300

    add comentario

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,8 +1,10 @@
 void setup() {
 pinMode(8,OUTPUT);
+//o pinmode é para definir a porta e também para definir entrada e saida
 }
 
 void loop() {
+//digitalwrite serve para mandar um sinal alto ou baixo para a porta designada
   digitalWrite(8, HIGH);
    delay(1000);
     digitalWrite(8, LOW);
