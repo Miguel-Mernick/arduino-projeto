014e22858fdf0b2ca2e540a8152db0813695e391
Miguel-Mernick <mernickmiguelsenai@gmail.com>
2024-08-06 08:22:13 -0300

    projeto para piscar 2 leds

--- piscar_2_leds/piscar_2_leds.ino
+++ piscar_2_leds/piscar_2_leds.ino
@@ -8,9 +8,9 @@ void setup() {
 void loop() {
  //digitalwrite serve para mandar um sinal alto ou baixo para a porta designada
  digitalWrite(8,HIGH);
- digitalWrite(7,HIGH);
+ digitalWrite(7,LOW);
  delay(1000);
  digitalWrite(8,LOW);
- digitalWrite(7,LOW);
+ digitalWrite(7,HIGH);
 delay(1000);
 }
